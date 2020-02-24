595fd4329556688eeec37ce86db2831717935e93
Ethan Knox <ethan.knox@health-union.com>
2020-02-24 16:06:53 -0500

    added using for pg,sf,bq

---
+++ test_xdb/models/under_test/using_test.sql
@@ -0,0 +1,31 @@
+WITH
+first_cte AS (
+	SELECT 
+        1 AS id
+        , 'banana' AS fruit
+        , 'sock' AS clothing
+
+),
+second_cte AS (
+    SELECT
+        1 AS id
+        , 'banana' AS fruit
+        , 'kaboom' AS batman_sound
+        , 'bubbles' AS another_thing
+    UNION ALL
+    SELECT
+        2 AS id
+        , 'orange' AS fruit
+        , 'kaboom' AS batman_sound
+        , 'bubbles' AS another_thing
+)
+
+SELECT 
+    count(*) AS number_of_rows
+FROM
+{{xdb.using('first_cte',
+            'second_cte',
+            'fruit')}}
+WHERE
+clothing = 'sock'
+
