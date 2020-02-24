2b008bd763e94f2a49179ad0e3f42bdadb00bad0
Ethan Knox <ethan.knox@health-union.com>
2020-02-24 14:36:34 -0500

    added any value for pg, sf, bq

---
+++ test_xdb/models/under_test/any_value_test.sql
@@ -0,0 +1,13 @@
+WITH
+values_of_things AS (
+	SELECT 1 AS value_col
+	UNION ALL 
+	SELECT 3 AS value_col
+	UNION ALL 
+	SELECT 5 AS value_col
+)
+
+SELECT 
+    {{xdb.any_value(value_col)}} AS value_col
+FROM
+    values_of_things
