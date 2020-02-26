ae6112a75f313686acdcea6380ec4c06f8739c8f
Ethan Knox <ethan.knox@health-union.com>
2020-02-26 09:50:52 -0500

    updated for bq support and added check for delimiter comma

--- macros/aggregate_strings.sql
+++ macros/aggregate_strings.sql
@@ -1,8 +1,10 @@
 {%- macro aggregate_strings(val, delim) -%}
-    {%- if target.type in ('bigquery','postgres',) -%} 
-        string_agg({{ val }}::varchar, {{ delim }})
-    {%- elif target.type in ('redshift','snowflake',) -%}
-        listagg({{ val }}::varchar, {{ delim }})
+    {%- if target.type ==  'postgres' -%} 
+        string_agg({{ val }}::varchar {{',' if delim else ''}}  {{ delim }})
+    {%- elif target.type == 'bigquery' -%}
+        string_agg(cast({{val}} as string) {{',' if delim else ''}}  {{ delim }})
+    {%- elif target.type == 'snowflake' -%}
+        listagg(cast({{ val }} as varchar){{',' if delim else ''}}  {{ delim }})
     {%- else -%}
         {{exceptions.raise_compiler_error("macro does not support aggregate_strings for target " ~ target.type ~ ".")}}
     {%- endif -%}
