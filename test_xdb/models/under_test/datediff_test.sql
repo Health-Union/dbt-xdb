e3b2a0e9760e2b422ccbff74af94f492db595717
Ethan Knox <ethan.knox@health-union.com>
2020-03-18 14:45:19 -0400

    breaking out for smaller merge

--- test_xdb/models/under_test/datediff_test.sql
+++ test_xdb/models/under_test/datediff_test.sql
@@ -7,5 +7,7 @@ arguments AS (
 
 SELECT 
     {{ xdb.datediff('day', "'2020-01-02'","base_arg") }} AS one_day_diff
+    ,{{ xdb.datediff('month',"'2020-01-15'","base_arg") }} AS two_week_diff
+    ,{{ xdb.datediff('month',"'2020-04-02'","base_arg") }} AS three_month_diff
 FROM
     arguments
