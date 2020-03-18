d0be302a76d88573a9cad529410e054f036a1213
Ethan Knox <ethan.knox@health-union.com>
2020-03-18 15:42:16 -0400

    added postgres support

--- macros/datediff.sql
+++ macros/datediff.sql
@@ -1,10 +1,11 @@
-{%- macro datediff(part, left_val, right_val) -%}
+{%- macro datediff(part, left_val, right_val, date_format='%Y-%m-%d') -%}
     {# determines the delta (in `part` units) between first_val and second_val.
        *Note* the order of left_val, right_val is reversed from Snowflake.
        ARGS:
          - part (string) one of 'day', 'week', 'month', 'year'
          - left_val (date/timestamp) the value before the minus in the equation "left - right"
          - right_val (date/timestamp) the value after the minus in the equation "left - right"
+         - date_format (pattern) a string pattern for the provided arguments (primarily for BigQuery)
        RETURNS: An integer representing the delta in `part` units
     #}
 
@@ -24,9 +25,16 @@
         {%- elif part == 'day' -%}
             DATE_PART('day', ({{left_val}}::TIMESTAMP - {{right_val}}::TIMESTAMP))
         {%- endif %}
+
     {%- elif target.type == 'bigquery' -%}
 
+        DATE_DIFF(PARSE_DATE('{{date_format}}', {{left_val}}), 
+                  PARSE_DATE('{{date_format}}', {{right_val}}), 
+                  {{part|upper}})
+
     {%- elif target.type == 'snowflake' -%}
+
+        DATE_DIFF({{part}}, {{right_val}},{{left_val}})
 
     {%- else -%}
         {{exceptions.raise_compiler_error("macro does not support datediff for target " ~ target.type ~ ".")}}
