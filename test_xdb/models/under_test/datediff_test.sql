ad30a506b33ed2a344d47b9edb5cbb75ff583a4d
Ethan Knox <ethan.knox@health-union.com>
2020-03-18 15:13:12 -0400

    added year test

--- test_xdb/models/under_test/datediff_test.sql
+++ test_xdb/models/under_test/datediff_test.sql
@@ -9,5 +9,6 @@ SELECT
     {{ xdb.datediff('day',"('2020-01-02'::DATE)","(base_arg::DATE)") }} AS one_day_diff
     ,{{ xdb.datediff('week',"'2020-01-16'","base_arg") }} AS two_week_diff
     ,{{ xdb.datediff('month',"'2020-04-02'","base_arg") }} AS three_month_diff
+    ,{{ xdb.datediff('year',"'2024-01-01'","base_arg") }} AS four_year_diff
 FROM
     arguments
