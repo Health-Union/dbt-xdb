b64b7af3ebc7cc8070f58e4a305a322457f805fe
Ethan Knox <ethan.knox@health-union.com>
2020-03-19 16:43:18 -0400

    macrodocs rips docs from macro comments

--- test_xdb/scripts/test_run.py
+++ test_xdb/scripts/test_run.py
@@ -1,14 +1,30 @@
 import yaml
 import subprocess
+import re
+import os
+from macrodocs import macrodocs
 
 
 ## run tests
 with open('./profiles.yml','r') as f:
     profile = yaml.safe_load(f.read())
-    
+success = 0
 for target in profile['default']['outputs']:
     print(f"\n\n~~~~~~~~~~~~~~~~~~ {target} ~~~~~~~~~~~~~~~~~~\n\n")
-    subprocess.call(['dbt','deps'])
-    subprocess.call(['dbt','run', '--profiles-dir','.','--target', target])
-    subprocess.call(['dbt','test', '--profiles-dir','.','--target', target])
-    
+    success += subprocess.call(['dbt','deps'])
+    success += subprocess.call(['dbt','run', '--profiles-dir','.','--target', target])
+    success += subprocess.call(['dbt','test', '--profiles-dir','.','--target', target])
+
+
+## build the docs once everything passes
+if success < 1:
+    header = """
+# XDB Available Macros
+
+These macros carry functionality across **Snowflake**, **Postgresql**, **Redshift** and **BigQuery** unless otherwise noted. 
+
+"""
+    macrodocs('/dbt-xdb/macros',
+              '/dbt-xdb/docs/macros.md',
+              header,
+              'xdb')
