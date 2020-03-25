41e4964e94dd2e77c62bb5437ea3677824abaa95
Ethan Knox <ethan.knox@health-union.com>
2020-03-25 12:20:24 -0400

    tests in place

---
+++ test_xdb/models/under_test/cast_timestamp_test.sql
@@ -0,0 +1,12 @@
+WITH 
+source_data AS (
+SELECT
+    '2020-01-01' AS date_col
+    ,'2020-01-01 20:20:20' AS timestamp_col
+)
+SELECT
+    {{xdb.cast_timestamp(date_col,'timestamp_ntz')}} AS date_ntz
+    ,{{xdb.cast_timestamp(date_col,'timestamp_tz')}} AS date_tz
+    ,{{xdb.cast_timestamp(timestamp_col,'timestamp_ntz')}} AS tstamp_ntz
+    ,{{xdb.cast_timestamp(timestamp_col,'timestamp_tz')}} AS tstamp_tz
+FROM source_data
