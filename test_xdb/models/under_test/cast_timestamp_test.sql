7969fe0abae58f9efc58b0e12c700712c20003ae
Ethan Knox <ethan.knox@health-union.com>
2020-03-25 15:15:38 -0400

    suppports all with tz and ntz but needs bigquery test flagging

--- test_xdb/models/under_test/cast_timestamp_test.sql
+++ test_xdb/models/under_test/cast_timestamp_test.sql
@@ -5,8 +5,8 @@ SELECT
     ,'2020-01-01 20:20:20' AS timestamp_col
 )
 SELECT
-    {{xdb.cast_timestamp(date_col,'timestamp_ntz')}} AS date_ntz
-    ,{{xdb.cast_timestamp(date_col,'timestamp_tz')}} AS date_tz
-    ,{{xdb.cast_timestamp(timestamp_col,'timestamp_ntz')}} AS tstamp_ntz
-    ,{{xdb.cast_timestamp(timestamp_col,'timestamp_tz')}} AS tstamp_tz
+    {{xdb.cast_timestamp('date_col','timestamp_ntz')}} AS date_ntz
+    ,{{xdb.cast_timestamp('date_col','timestamp_tz')}} AS date_tz
+    ,{{xdb.cast_timestamp('timestamp_col','timestamp_ntz')}} AS tstamp_ntz
+    ,{{xdb.cast_timestamp('timestamp_col','timestamp_tz')}} AS tstamp_tz
 FROM source_data
