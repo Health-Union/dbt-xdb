800c3fe9808aa797f3cb9bf8c3621ed383392c34
Ethan Knox <ethan.knox@health-union.com>
2020-03-26 16:42:13 -0400

    months pg

--- test_xdb/models/under_test/dateadd_test.sql
+++ test_xdb/models/under_test/dateadd_test.sql
@@ -9,6 +9,8 @@ SELECT
     ,{{xdb.dateadd('day','-1', 'date_col')}} as one_day_subtracted
     ,{{xdb.dateadd('week','1', 'date_col')}} as one_week_added
     ,{{xdb.dateadd('week','-1', 'date_col')}} as one_week_subtracted
+    ,{{xdb.dateadd('month','1', 'date_col')}} as one_month_added
+    ,{{xdb.dateadd('month','-1', 'date_col')}} as one_month_subtracted
 FROM
     source_data
 
