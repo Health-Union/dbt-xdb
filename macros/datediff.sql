2c48cc8c69313ddc21a0389f4e18eb373e950ac4
Dylan Renzulli <dylan.renzulli@health-union.com>
2020-03-27 14:05:51 -0400

    Updated to include second, minute and hour options for datediff.

--- macros/datediff.sql
+++ macros/datediff.sql
@@ -2,14 +2,14 @@
     {# determines the delta (in `part` units) between first_val and second_val.
        *Note* the order of left_val, right_val is reversed from Snowflake.
        ARGS:
-         - part (string) one of 'day', 'week', 'month', 'year', 'quarter'
+         - part (string) one of 'second', 'minute', 'hour', 'day', 'week', 'month', 'year', 'quarter'
          - left_val (date/timestamp) the value before the minus in the equation "left - right"
          - right_val (date/timestamp) the value after the minus in the equation "left - right"
          - date_format (pattern) a string pattern for the provided arguments (primarily for BigQuery)
        RETURNS: An integer representing the delta in `part` units
     #}
     {%- set part = part |lower -%}
-    {%if part not in ('day','week','month','year','quarter',) %}
+    {%if part not in ('second', 'minute', 'hour', 'day','week','month','year','quarter',) %}
         {{exceptions.raise_compiler_error("macro datediff for target does not support date part value " ~ part)}}
     {%- endif -%}
 
@@ -28,13 +28,31 @@
             TRUNC(DATE_PART('day', ({{left_val}}::TIMESTAMP - {{right_val}}::TIMESTAMP))/7)
         {%- elif part == 'day' -%}
             DATE_PART('day', ({{left_val}}::TIMESTAMP - {{right_val}}::TIMESTAMP))
+        {%- elif part =='hour' -%}
+            {{ xdb.datediff('day', left_val, right_val) }} * 24 
+            + 
+            DATE_PART('hour', {{left_val}}::TIMESTAMP - {{right_val}}::TIMESTAMP)
+        {%- elif part =='minute' -%}
+            {{ xdb.datediff('hour', left_val, right_val) }} * 60
+            + 
+            DATE_PART('minute', {{left_val}}::TIMESTAMP - {{right_val}}::TIMESTAMP)
+        {%- elif part =='second' -%}
+            {{ xdb.datediff('minute', left_val, right_val) }} * 60
+            + 
+            DATE_PART('second', {{left_val}}::TIMESTAMP - {{right_val}}::TIMESTAMP)
         {%- endif %}
 
-    {%- elif target.type == 'bigquery' -%}
 
-        DATE_DIFF(PARSE_DATE('{{date_format}}', {{left_val}}), 
-                  PARSE_DATE('{{date_format}}', {{right_val}}), 
-                  {{part|upper}})
+    {%- elif target.type == 'bigquery' -%}
+        {% if part in ('second', 'minute', 'hour') %}
+            TIMESTAMP_DIFF(PARSE_TIMESTAMP('{{date_format}}', FORMAT_TIMESTAMP('{{date_format}}', {{ xdb.cast_timestamp(left_val, 'timestamp') }})), 
+                      PARSE_TIMESTAMP('{{date_format}}', FORMAT_TIMESTAMP('{{date_format}}', {{ xdb.cast_timestamp(right_val, 'timestamp') }})), 
+                      {{part|upper}})
+        {% else %}
+            DATE_DIFF(PARSE_DATE('{{date_format}}', {{left_val}}), 
+                      PARSE_DATE('{{date_format}}', {{right_val}}), 
+                      {{part|upper}})
+        {%- endif %}
 
     {%- elif target.type == 'snowflake' -%}
 
