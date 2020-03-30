9201a16db41927562750a632e6a777cbf0848c3c
Ethan Knox <ethan.knox@health-union.com>
2020-03-30 10:21:54 -0400

    tests set up for quote insensitive

---
+++ test_xdb/models/under_test/quote_insensitive_test.sql
@@ -0,0 +1,10 @@
+SELECT 
+    {% for val in ('SnOwFlakE','BigQuerY','PostGRes',) %}
+        {% if val == target.type %}
+            '{{xdb.quote_insensitive(val)}}' AS folded_not_reserved_word_camel
+        {% endif %}
+    {% endfor %}
+    ,'{{xdb.quote_insensitive(target.type|lower)}}' AS folded_not_reserved_word_upper
+    ,'{{xdb.quote_insensitive("group")}}' AS folded_reserved_word
+
+
