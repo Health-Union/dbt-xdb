1d4cb86af66958893e793299a51e5d5e0ff56a49
ethan knox <ethan.m.knox@gmail.com>
2020-04-22 08:52:23 -0400

    solution for quiet compile

---
+++ macros/utilities/not_supported_exception.sql
@@ -0,0 +1,16 @@
+{%- macro _not_supported_exception(macro_name) -%} {# no_coverage #}
+    /*{# Throws an exception unless the global `xdb_allow_unsupported_macros` isTrue.
+        ARGS:
+         - macro_name (string): the name of the macro throwing the exception.
+        RETURNS: None
+    #}*/
+    {%- if not var("xdb_allow_unsupported_macros", False) -%}
+	    {{exceptions.raise_compiler_error("macro " ~ macro_name ~ " is not supported for target " ~ target.type ~ ".")}}
+    {%- else -%}
+        -- unsupported macro {{macro_name}} ignored.
+    {%- endif -%}
+{%- endmacro -%}  
+
+{%- macro not_supported_exception(macro_name) -%} {# no_coverage #}
+    {{xdb.strip_to_single_line(xdb._not_supported_exception(macro_name))}}
+{%- endmacro -%}
