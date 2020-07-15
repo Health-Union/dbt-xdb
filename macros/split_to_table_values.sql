f96afeb7bf3364448c675239487375cd0b72e451
Dylan Renzulli <dylan.renzulli@health-union.com>
2020-07-15 13:48:14 -0400

    Updated the split to add unnest_values, changing split_to_table_values as a wrapper macro

--- macros/split_to_table_values.sql
+++ macros/split_to_table_values.sql
@@ -1,14 +1,13 @@
 {%- macro split_to_table_values(table_array) -%}
     {# Used in conjunction with split_to_table, this macro returns the split_to_table
-        values associated with the split_to_table function
-       ARGS:
+        values associated with the split_to_table macro
+    NOTE: This is a wrapper macro for unnest_values.
+    ARGS:
          - table_array (string) the table array form of the split_column.
        RETURNS: A new column containing the split data.
     #}
-    {%- if target.type in ['postgres', 'bigquery'] -%} 
-        {{ table_array }}
-    {%- elif target.type == 'snowflake' -%}
-        {{ table_array }}.value::VARCHAR
+    {%- if target.type in ['postgres', 'bigquery', 'snowflake'] -%} 
+        {{ xdb.unnest_values(table_array) }}
     {%- else -%}
         {{ xdb.not_supported_exception('split_to_table_values') }}
     {%- endif -%}
