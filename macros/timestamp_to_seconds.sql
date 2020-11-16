b7437c0f50250810eb113f8affa50c0fbc347bf6
Phil Ribbens <philip.ribbens@health-union.com>
2020-11-16 16:25:29 -0500

    make timestamp_to_seconds more general

--- macros/timestamp_to_seconds.sql
+++
@@ -1,9 +0,0 @@
-{% macro timestamp_to_seconds(timestamp_t) %}
-  {%- if target.type == 'postgres' -%}
-    EXTRACT(epoch from {{ timestamp_t }})
-  {%- elif target.type == 'snowflake' -%}
-    TO_DOUBLE(EXTRACT(epoch from {{ timestamp_t }}))
-  {%- else -%}
-        {{ xdb.not_supported_exception('timestamp_to_seconds') }}
-  {%- endif -%}
-{% endmacro %}
