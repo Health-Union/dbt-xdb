c0ce77087bf69835a2f4b3bb5c0d5f5c7a4d174e
Phil Ribbens <philip.ribbens@health-union.com>
2020-11-18 16:16:45 -0500

    add comments to recently added macros

--- macros/timestamp_to_date_part.sql
+++ macros/timestamp_to_date_part.sql
@@ -1,4 +1,10 @@
 {% macro timestamp_to_date_part(timestamp_t, date_part) %}
+  {# Ensures that result of EXTRACT in Snowflake is a double to match the default behavior of EXTRACT in postgres
+      ARGS:
+        - timestamp_t (timestamp): timestamp to extract the date_part from 
+        - date_part (string): tested for 'epoch', 'year', 'month', 'day', 'hour', 'minute', 'second'
+      RETURNS: double 
+  #}
   {%- if target.type == 'postgres' -%}
     EXTRACT({{ date_part }} from {{ timestamp_t }})
   {%- elif target.type == 'snowflake' -%}
