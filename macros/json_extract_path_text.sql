93453e752f1d72dc29ce60ddf1dbc408857d4671
Chris Ulicny <chris.ulicny@health-union.com>
2021-03-24 14:04:20 -0400

    Switched to use json_extract_path_text func. Added tests

--- macros/json_extract_path_text.sql
+++ macros/json_extract_path_text.sql
@@ -1,4 +1,4 @@
-{% macro extract_json_path(column, path_vals) %}
+{% macro json_extract_path_text(column, path_vals) %}
   {#
     Extracts the value at `path_vals` from the json typed `column` (or expression)
        ARGS:
@@ -8,35 +8,31 @@
            Integer values are assumed to index arrays
            Integers that are typed as strings are assumed to be keys
 
+           Note that in some DB cases it does not matter the typing:
+            - postgres: '0' will indicate the key "0" or first array item based on context
+
        RETURNS: (varchar) The value at the given path in the column value
   #}
   {% if target.type == 'postgres' %}
-    {% set expr = namespace(value=column) %}
+    {% set expr = namespace(value="json_extract_path_text(" ~ column ~ ", ") %}
     {% for val in path_vals %}
-      {% if val is integer %}
-        {% set append_val = val %}
-      {% else %}
-        {% set append_val = "'" ~ val ~ "'" %}
-      {% endif %}
-
-      {% if loop.last %}
-        {% set expr.value = expr.value ~ '->>' ~ append_val %}
-      {% else %}
-        {% set expr.value = expr.value ~ '->' ~ append_val %}
+      {% set expr.value = expr.value ~ "'" ~ append_val ~ "'" %}
+      {% if not loop.last %}
+        {% set expr.value = expr.value ~ ", " %}
       {% endif %}
     {% endfor %}
+    {% set expr.value = expr.value ~ ")" %}
   {% elif target.type == 'snowflake' %}
-    {% set expr = namespace(value=column ~ ':') %}
+    {% set expr = namespace(value="json_extract_path_text(" ~ column ~ ", '") %}
     {% for val in path_vals %}
       {% if val is integer %}
         {% set append_val = val %}
         {% set expr.value = expr.value ~ '[' ~ append_val ~ ']' %}
       {% else %}
-        {% set append_val = "'" ~ val ~ "'" %}
-        {% set expr.value = expr.value ~ '.' ~ append_val %}
+        {% set expr.value = expr.value ~ '."' ~ append_val ~ '"' %}
       {% endif %}
     {% endfor %}
-    {% set expr.value = expr.value ~ '::text' %}
+    {% set expr.value = expr.value ~ ')' %}
   {% else %}
     {{raise_not_supported_error()}}
   {% endif %}
---
+++ test_xdb/models/under_test/json_extract_path_text_test.sql
@@ -0,0 +1,22 @@
+{{ config({"tags":["exclude_bigquery", "exclude_bigquery_tests"]}) }}
+
+with test_json_data as (
+    select
+        {% set js = '{"key1":"value1", "key2":{"innerkey2_1":"innervalue2_1", "0":[0, null, "string"]}, "key3": -1234}' %}
+        {% if target.type == 'postgres' %}'{{js}}'::json
+        {% elif target.type == 'snowflake' %}parse_json('{{js}}')
+        {% endif %}
+            as json_col
+)
+select
+    json_col,
+    {{xdb.json_extract_path_text('json_col', ['key1'])}} as key1__val,
+    {{xdb.json_extract_path_text('json_col', ['key2'])}} as key2__val,
+    {{xdb.json_extract_path_text('json_col', ['key2', 'innerkey2_1'])}} as key2_innerkey2_1__val,
+    {{xdb.json_extract_path_text('json_col', ['key2', '0'])}} as key2_0__val,
+    {{xdb.json_extract_path_text('json_col', ['key2', '0', 0])}} as key2_0__val_0,
+    {{xdb.json_extract_path_text('json_col', ['key2', '0', 1])}} as key2_0__val_1,
+    {{xdb.json_extract_path_text('json_col', ['key2', '0', 2])}} as key2_0__val_2,
+    {{xdb.json_extract_path_text('json_col', ['key3'])}} as key3__val,
+    cast({{xdb.json_extract_path_text('json_col', ['key3'])}} as int) as key3__val_int
+from test_json_data
