d826a0535ac2fe57d3e8378f7e269318ab999cea
Chris Ulicny <chris.ulicny@health-union.com>
2021-03-25 14:01:53 -0400

    Added comments about new test setup

--- test_xdb/scripts/gather_targets.py
+++ test_xdb/scripts/gather_targets.py
@@ -5,6 +5,8 @@ def gather_targets(specified_targets=list()):
     with open('./profiles.yml','r') as f:
         profile = yaml.safe_load(f.read())
 
+    # handle any target specific setup that is needed (e.g. postgres extensions)
     if 'postgres' in specified_targets or len(specified_targets) == 0:
         prepare_postgres_db(profile['default']['outputs']['postgres'])
+
     return specified_targets if specified_targets else profile['default']['outputs']
--- test_xdb/scripts/test_setup.py
+++ test_xdb/scripts/test_setup.py
@@ -10,6 +10,11 @@ for target in profile['default']['outputs']:
 
 
 def prepare_postgres_db(profile: dict):
+    """ Prepares the target postgres db for testing
+
+        Args:
+            - profile (dict): The loaded postgres yaml block from profiles.yml
+    """
     user = profile['user']
     password = profile['pass']
     host = profile['host']
@@ -17,5 +22,6 @@ def prepare_postgres_db(profile: dict):
     db = profile['dbname']
     engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
     with engine.connect() as conn:
+        # install uuid-ossp for generating uuid fields
         conn.execute('create extension if not exists "uuid-ossp"')
     engine.dispose()
