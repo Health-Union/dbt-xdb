cef0ec1479518eb11221b89ced1cb370f9cb4696
Dylan Renzulli <dylan.renzulli@health-union.com>
2021-04-19 13:58:14 -0400

    Added array_contains to xdb for snowflake and postgress

---
+++ test_xdb/models/under_test/array_contains_test.sql
@@ -0,0 +1,31 @@
+WITH starter_values AS (
+	SELECT 0 AS index_column, 'a' AS val
+	UNION ALL 
+	SELECT 0 AS index_column, 'b' AS val
+	UNION ALL 
+	SELECT 0 AS index_column, 'c' AS val
+    UNION ALL
+    SELECT 1 AS index_cloumn, 'd' AS val
+    UNION ALL
+    SELECT 1 AS index_cloumn, 'e' AS val
+    UNION ALL
+    SELECT 1 AS index_cloumn, 'f' AS val
+    UNION ALL
+    SELECT 2 AS index_cloumn, 'g' AS val
+    UNION ALL
+    SELECT 2 AS index_cloumn, 'h' AS val
+    UNION ALL
+    SELECT 2 AS index_cloumn, 'i' AS val
+
+)
+
+, array_agg_values AS (
+    SELECT index_column
+    , ARRAY_AGG(val) AS array_values
+    FROM starter_values
+    GROUP BY 1
+)
+
+SELECT index_column AS value_col
+FROM array_agg_values
+WHERE {{ xdb.array_contains('array_values', 'e') }}
