a9cf94ddd567b65591d01f3e9bc9111d41c79b55
Artem Karpenko <artem.karpenko@health-union.com>
2022-05-12 17:12:56 +0300

    DATA-2618: update the private macro name

--- macros/env_generate_schema_name.sql
+++ macros/env_generate_schema_name.sql
@@ -19,13 +19,13 @@
     {%- endif -%}
   {%- else -%}
     {%- if custom_schema_name is none -%}
-      {{ xdb._dev_schema(branch_name) }}_{{ default_schema }}
+      {{ xdb._normalize_schema(branch_name) }}_{{ default_schema }}
     {%- else -%}
-      {{ xdb._dev_schema(branch_name) }}_{{ default_schema }}_{{ custom_schema_name | trim }}
+      {{ xdb._normalize_schema(branch_name) }}_{{ default_schema }}_{{ custom_schema_name | trim }}
     {%- endif -%}
   {%- endif -%}
 {%- endmacro %}
 
-{%- macro _dev_schema(branch_name) -%}
+{%- macro _normalize_schema(branch_name) -%}
   {{ branch_name | replace('/','') | replace('-','') | replace('_','') | lower}}
 {%- endmacro -%}
