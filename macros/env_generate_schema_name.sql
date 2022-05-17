9c033583013d6a89787d69066cb2600775feff1d
Artem Karpenko <artem.karpenko@health-union.com>
2022-05-17 18:58:11 +0300

    DATA-2618: some fixes to custom_schema_name case

--- macros/env_generate_schema_name.sql
+++ macros/env_generate_schema_name.sql
@@ -15,7 +15,7 @@
     {%- if custom_schema_name is none -%}
       {{ default_schema }}
     {%- else -%}
-      {{ default_schema }}_{{ custom_schema_name | trim }}
+      {{ custom_schema_name | trim }}
     {%- endif -%}
   {%- else -%}
     {%- if custom_schema_name is none -%}
