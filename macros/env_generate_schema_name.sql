af8f03e136f42f6b9aa1efc6bb66e8fa9847e1cd
Artem Karpenko <artem.karpenko@health-union.com>
2022-05-18 17:33:35 +0300

    DATA-2618: adjust the snapshot dev schema case

--- macros/env_generate_schema_name.sql
+++ macros/env_generate_schema_name.sql
@@ -21,7 +21,7 @@
     {%- if custom_schema_name is none -%}
       {{ xdb._normalize_schema(branch_name) }}_{{ default_schema }}
     {%- else -%}
-      {{ xdb._normalize_schema(branch_name) }}_{{ default_schema }}_{{ custom_schema_name | trim }}
+      {{ xdb._normalize_schema(branch_name) }}_{{ custom_schema_name | trim }}
     {%- endif -%}
   {%- endif -%}
 {%- endmacro %}
