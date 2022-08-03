8d96a717e7d4e65d8466e44a533b43224b500734
Oleg Kolesnikov <oleg.kolesnikov@health-union.com>
2022-08-03 14:25:53 +0300

    rollback of changes in cast_timestamp() macro, update of timeadd() macro test

--- macros/cast_timestamp.sql
+++ macros/cast_timestamp.sql
@@ -16,9 +16,9 @@
 
 {%- if target.type ==  'postgres' -%} 
     {%- if cast_as in ('timestamp_ntz','timestamp') -%}
-        {{val}}::TIMESTAMP WITH TIME ZONE AT TIME ZONE 'UTC'
+        {{val}}::TIMESTAMP
     {%- elif cast_as == 'timestamp_tz' -%}
-        {{val}}::TIMESTAMP WITH TIME ZONE
+        {{val}}::TIMESTAMP AT TIME ZONE 'UTC'
     {%- endif -%}
 
 {%- elif target.type == 'bigquery' -%}
--- test_xdb/models/under_test/timeadd_test.sql
+++ test_xdb/models/under_test/timeadd_test.sql
@@ -1,3 +1,5 @@
+{{ config({"tags":["exclude_bigquery", "exclude_bigquery_tests"]}) }}
+
 WITH
 source_data AS (
     SELECT 
@@ -5,6 +7,7 @@ source_data AS (
         , '2020-01-01 00:00:00'::TIMESTAMP AS date_col_timestamp
 )
 
+, string_handled_data AS (
 SELECT
 --cast to timestamp logic block check
     {{xdb.timeadd('second', '1', 'date_col_string')}} AS one_second_added
@@ -13,13 +16,27 @@ SELECT
     , {{xdb.timeadd('minute', '-1', 'date_col_string')}} AS one_minute_subtracted
     , {{xdb.timeadd('hour', '1', 'date_col_string')}} AS one_hour_added
     , {{xdb.timeadd('hour', '-1', 'date_col_string')}} AS one_hour_subtracted
---NO cast to timestamp logic block check
-    , {{xdb.timeadd('second', '1', 'date_col_timestamp', false)}} AS no_cast_one_second_added
-    , {{xdb.timeadd('second', '-1', 'date_col_timestamp', false)}} AS no_cast_one_second_subtracted
-    , {{xdb.timeadd('minute', '1', 'date_col_timestamp', false)}} AS no_cast_one_minute_added
-    , {{xdb.timeadd('minute', '-1', 'date_col_timestamp', false)}} AS no_cast_one_minute_subtracted
-    , {{xdb.timeadd('hour', '1', 'date_col_timestamp', false)}} AS no_cast_one_hour_added
-    , {{xdb.timeadd('hour', '-1', 'date_col_timestamp', false)}} AS no_cast_one_hour_subtracted
 FROM
     source_data
+)
 
+, timestamp_handled_data AS (
+SELECT
+--NO cast to timestamp logic block check
+    , {{xdb.timeadd('second', '1', 'date_col_timestamp', false)}} AS one_second_added
+    , {{xdb.timeadd('second', '-1', 'date_col_timestamp', false)}} AS one_second_subtracted
+    , {{xdb.timeadd('minute', '1', 'date_col_timestamp', false)}} AS one_minute_added
+    , {{xdb.timeadd('minute', '-1', 'date_col_timestamp', false)}} AS one_minute_subtracted
+    , {{xdb.timeadd('hour', '1', 'date_col_timestamp', false)}} AS one_hour_added
+    , {{xdb.timeadd('hour', '-1', 'date_col_timestamp', false)}} AS one_hour_subtracted
+FROM
+    source_data
+)
+
+, unioned_data AS (
+    SELECT * FROM timestamp_handled_data
+    UNION
+    SELECT * FROM string_handled_data
+)
+
+SELECT * FROM unioned_data
