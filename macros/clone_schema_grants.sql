b68452a543cae94da30386d1681e9d0cdb599f71
Oleg Kolesnikov <oleg.kolesnikov@health-union.com>
2023-06-13 01:15:54 +0300

    Update of clone_schema_grants() macro.

--- macros/clone_schema_grants.sql
+++ macros/clone_schema_grants.sql
@@ -57,7 +57,7 @@
         {% set revoke_grants = run_query(get_revoke_grants) %}
 
         {#/*3. Cloning ownership, revoke current grants and applying target ones that are on `schema_one` to `schema_two`.*/#}
-        {% set sql %}
+        {% set sql_set_grants %}
                 {{"ALTER SCHEMA " ~ schema_two ~ " OWNER TO " ~ target_owner ~ ";"}}
             {% for i in target_grants %}
                 {{"GRANT " ~ i[0] ~ " ON SCHEMA " ~ schema_two ~ " TO " ~ i[1] ~ ";"}}
@@ -70,75 +70,104 @@
     {%- elif target.type == 'snowflake' -%}
 
         {#/*1. Scanning for grants on `schema_one`.*/#}
-        {% set scan_grants %}
-            SHOW GRANTS ON SCHEMA {{schema_one}};
-        {% endset %}
-        {% do run_query(scan_grants) %}
+        {% set query_to_get_source_grants = xdb._get_schema_grants_query(schema_one) %}
+        {% set source_grants = run_query(query_to_get_source_grants) %}
 
-        {#/*2. Fetching scan query ID.*/#}
-        {% if '.' in schema_one %}
-        {% set database_one = schema_one.split('.')[0] %}
-        {% else %}
-            {% if execute %}
-                {% set get_database %}
-                    SELECT CURRENT_DATABASE();
-                {% endset %}
-                {% set database_one = run_query(get_database)[0][0] %}
-                {% else %}
-                {% set database_one = '' %}
-                {% endif %}
-        {% endif %}
-        {% set get_scan_query_id %}
-            SELECT query_id
-            FROM TABLE({{database_one}}.INFORMATION_SCHEMA.QUERY_HISTORY())
-            WHERE query_text = 'SHOW GRANTS ON SCHEMA {{schema_one}};'
-            ORDER BY start_time DESC LIMIT 1
-        {% endset %}
-        {% if execute %}
-        {% set scan_query_id = run_query(get_scan_query_id)[0][0] %}
-        {% else %}
-        {% set scan_query_id = '' %}
-        {% endif %}
-
-        {#/*3. Fetching grants details.*/#}
-        {% set get_target_grants %}
-            SELECT "privilege"
-                ,"granted_on"
-                ,"granted_to"
-                ,"grantee_name"
-                , CASE WHEN "privilege" = 'OWNERSHIP' THEN 1 ELSE 2 END AS order_flag
-            FROM (SELECT * FROM TABLE(RESULT_SCAN('{{scan_query_id}}')))
-            WHERE "privilege" NOT IN ('CREATE ANOMALY_DETECTION', 'CREATE STATEFUL_FORECAST', 'CREATE BUDGET')
-            --This condition is a work around to avoid an error by this link -  https://cloud.getdbt.com/deploy/159/projects/3671/runs/149552418
-            --which is related to invention of 3 new privileges: 'CREATE ANOMALY_DETECTION', 'CREATE STATEFUL_FORECAST', 'CREATE BUDGET' which
-            --can't be either granted or revoked by a special separate command, but can be provided by ALL-syntax.
-            --It might be removed after having grant and revoke options for them in some future.
-            ORDER BY 5
-        {% endset %}
-        {% set target_grants = run_query(get_target_grants) %}
-
-        {#/*4. Cloning ownership and applying grants that are on `schema_one` to `schema_two`.*/#}
-        {% set sql %}
-            {% for i in target_grants %}
+        {#/*2. Cloning ownership that are on `schema_one` to `schema_two`.*/#}
+        {% set sql_set_ownership %}
+            {% for i in source_grants %}
                 {%- if i[0] == 'OWNERSHIP' -%}
-                    {{"GRANT " ~ i[0] ~ " ON "  ~ i[1] ~ " " ~ schema_two ~ " TO " ~ i[2] ~ " " ~ i[3] ~ " REVOKE CURRENT GRANTS;"}}
-                    {{"USE ROLE " ~ i[3] ~ ";"}}
-                    {{"GRANT ALL ON "  ~ i[1] ~ " " ~ schema_two ~ " TO " ~ i[2] ~ " " ~ i[3] ~ ";"}}
-                    {#/*This GRANT ALL command is a work around to avoid an error by this link -  https://cloud.getdbt.com/deploy/159/projects/3671/runs/149552418.
-                        See the previous comment.*/#}
-                {%- else -%}
-                     {{"GRANT " ~ i[0] ~ " ON "  ~ i[1] ~ " " ~ schema_two ~ " TO " ~ i[2] ~ " " ~ i[3] ~ ";"}}
+                {{"GRANT " ~ i[0] ~ " ON "  ~ i[1] ~ " " ~ schema_two ~ " TO " ~ i[2] ~ " " ~ i[3] ~ " REVOKE CURRENT GRANTS;"}}
+                {{"USE ROLE " ~ i[3] ~ ";"}}
+                {{"GRANT ALL ON "  ~ i[1] ~ " " ~ schema_two ~ " TO " ~ i[2] ~ " " ~ i[3] ~ ";"}}
                 {%- endif -%}
             {% endfor %}
+        {% endset %}
+        {% do run_query(sql_set_ownership) %}
+
+        {#/*3. Scanning for current grants on `schema_two`.*/#}
+        {% set query_to_get_target_grants = xdb._get_schema_grants_query(schema_two) %}
+        {% set target_grants = run_query(query_to_get_target_grants) %}
+
+        {#/*4. Applying remaining grants to `schema_two`.*/#}
+        {% set sql_set_grants %}
+            {% for i in source_grants %}
+                {% set already_present_grant_flag = false %}
+                {%- if i[0] != 'OWNERSHIP' -%}
+                    {% for j in target_grants %}
+                        {%- if i[0] == j[0] and i[1] == j[1] and i[2] == j[2] and i[3] == j[3] -%}
+                            {% set already_present_grant_flag = true %}
+                            {{'--'}}
+                        {%- endif -%}
+                    {% endfor %}
+                    {%- if already_present_grant_flag == false -%}
+                    {{"GRANT " ~ i[0] ~ " ON "  ~ i[1] ~ " " ~ schema_two ~ " TO " ~ i[2] ~ " " ~ i[3] ~ ";"}}
+                    {%- endif -%}
+                {%- endif -%}
+            {% endfor %}
+            {#/*This is a default workable statement to avoid having attempts of running empty list of commands.*/#}
+            {{"SELECT 1;"}}
         {% endset %}
 
     {%- else -%}
         {{ xdb.not_supported_exception('The clone_schema_grants() macro doesn`t support this type of database target.') }}
     {%- endif -%}
 
-    {% do run_query(sql) %}
+    {#/*Fetching of final list of commands from `sql_set_grants` var.*/#}
+    {% do run_query(sql_set_grants) %}
     {{ log("Grants that are on `schema_one` have been applied on `schema_two` successfully.", info=True) }}
 
 {% endmacro %}
 
- by ones on schema `schema_one`
+{% macro _get_schema_grants_query(schema_name) %}
+    {#/*
+        This is an auxiliary macro for generating query which provides details
+        about applied grants on schema `schema_name` after being fetched.
+        SUPPORTS:
+            - Snowflake
+    */#}
+    {#/*1. Scanning for grants on `schema_name`.*/#}
+    {% set scan_grants %}
+        SHOW GRANTS ON SCHEMA {{schema_name}};
+    {% endset %}
+    {% do run_query(scan_grants) %}
+
+    {#/*2. Fetching scan query ID.*/#}
+    {% if '.' in schema_name %}
+    {% set database_one = schema_name.split('.')[0] %}
+    {% else %}
+        {% if execute %}
+            {% set get_database %}
+                SELECT CURRENT_DATABASE();
+            {% endset %}
+            {% set database_one = run_query(get_database)[0][0] %}
+            {% else %}
+            {% set database_one = '' %}
+            {% endif %}
+    {% endif %}
+    {% set get_scan_query_id %}
+        SELECT query_id
+        FROM TABLE({{database_one}}.INFORMATION_SCHEMA.QUERY_HISTORY())
+        WHERE query_text = 'SHOW GRANTS ON SCHEMA {{schema_name}};'
+        ORDER BY start_time DESC LIMIT 1
+    {% endset %}
+    {% if execute %}
+    {% set scan_query_id = run_query(get_scan_query_id)[0][0] %}
+    {% else %}
+    {% set scan_query_id = '' %}
+    {% endif %}
+
+    {#/*3. Building a query to be fetched.*/#}
+    {% set query %}
+        SELECT "privilege"
+            ,"granted_on"
+            ,"granted_to"
+            ,"grantee_name"
+            , CASE WHEN "privilege" = 'OWNERSHIP' THEN 1 ELSE 2 END AS order_flag
+        FROM (SELECT * FROM TABLE(RESULT_SCAN('{{scan_query_id}}')))
+        ORDER BY 5
+    {% endset %}
+
+    {{ return(query) }}
+
+{%- endmacro -%}
