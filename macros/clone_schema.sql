817ee181f862b70bca8bc68a759da4ea791a580b
Oleg Kolesnikov <oleg.kolesnikov@health-union.com>
2024-01-04 22:49:52 +0200

    Adding ordering of objects to clone_schema() macro for correct handling of creation of views #2.

--- macros/clone_schema.sql
+++ macros/clone_schema.sql
@@ -267,40 +267,65 @@
         {%- if comment_tag == '' -%}
             {% set fetch_tagged_objects %}
                 USE SCHEMA {{schema_one}};
-                SELECT
-                    CASE WHEN is_transient = 'YES' THEN 'TRANSIENT TABLE' ELSE 'TABLE' END AS type
-                    , table_name AS name
-                    , NULL AS object_definition
-                    , coalesce(comment, '-1') AS comment
-                FROM information_schema.tables
-                WHERE LOWER(table_schema) = LOWER('{{schema_one}}')
-                    AND table_type = 'BASE TABLE'
-                UNION ALL
-                SELECT
-                    'VIEW' AS type
-                    , table_name AS name
-                    , view_definition AS object_definition
-                    , coalesce(comment, '-1') AS comment
-                FROM information_schema.views
-                WHERE LOWER(table_schema) = LOWER('{{schema_one}}')
-                UNION ALL
-                SELECT
-                    'SEQUENCE' AS type
-                    , sequence_name AS name
-                    , NULL AS object_definition
-                    , coalesce(comment, '-1') AS comment
-                FROM information_schema.sequences
-                WHERE LOWER(sequence_schema) = LOWER('{{schema_one}}')
-                {% if functions_names is defined -%}
-                    {% for i in functions_names -%}
-                UNION ALL
-                SELECT
-                    'FUNCTION' AS type
-                    , '{{i[0]}}' AS name
-                    , get_ddl('function', '{{i[0]}}') AS object_definition
-                    , '{{i[1]}}' AS comment
-                    {% endfor %}
-                {%- endif -%}
+                SELECT * FROM (
+                    SELECT
+                        CASE WHEN is_transient = 'YES' THEN 'TRANSIENT TABLE' ELSE 'TABLE' END AS type
+                        , table_name AS name
+                        , NULL AS object_definition
+                        , coalesce(comment, '-1') AS comment
+                        , 0 AS order_rank
+                    FROM information_schema.tables
+                    WHERE LOWER(table_schema) = LOWER('{{schema_one}}')
+                        AND table_type = 'BASE TABLE'
+                    UNION ALL
+                    SELECT * FROM (
+                            WITH views_data AS (
+                                SELECT
+                                    'VIEW' AS type
+                                    , table_name AS name
+                                    , view_definition AS object_definition
+                                    , coalesce(comment, '-1') AS comment
+                                FROM information_schema.views
+                                WHERE LOWER(table_schema) = LOWER('{{schema_one}}')
+                                    AND LOWER(comment) = LOWER('{{comment_tag}}')
+                            )
+                            , names AS (
+                                SELECT NAME AS view_name FROM views_data
+                                )
+                            SELECT
+                                views_data.TYPE
+                                , views_data.NAME
+                                , views_data.object_definition
+                                , views_data.comment
+                                , SUM(CASE 
+                                        WHEN CONTAINS(lower(object_definition), lower(view_name))
+                                            AND VIEW_NAME <> NAME THEN 1
+                                        ELSE 0
+                                    END) AS order_rank
+                            FROM views_data, names
+                            GROUP BY 1, 2, 3, 4
+                        ) views
+                    UNION ALL
+                    SELECT
+                        'SEQUENCE' AS type
+                        , sequence_name AS name
+                        , NULL AS object_definition
+                        , coalesce(comment, '-1') AS comment
+                        , 0 AS order_rank
+                    FROM information_schema.sequences
+                    WHERE LOWER(sequence_schema) = LOWER('{{schema_one}}')
+                    {% if functions_names is defined -%}
+                        {% for i in functions_names -%}
+                    UNION ALL
+                    SELECT
+                        'FUNCTION' AS type
+                        , '{{i[0]}}' AS name
+                        , get_ddl('function', '{{i[0]}}') AS object_definition
+                        , '{{i[1]}}' AS comment
+                        , 0 AS order_rank
+                        {% endfor %}
+                    {%- endif -%}
+                ) ORDER BY order_rank ASC;
             {% endset %}
         {%- else -%}
             {% set fetch_tagged_objects %}
